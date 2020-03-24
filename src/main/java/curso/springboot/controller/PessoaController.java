67edcb7ad52ff022d88758eca952a93a2f453a41
Allan <allan_f2008@hotmail.com>
2020-03-24 20:12:25 -0300

    ajustes layout

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
@@ -70,6 +72,18 @@ public class PessoaController {
 		andView.addObject("pessoas", PessoaRepository.findAll());
 		andView.addObject("pessoaobj", new Pessoa());
 			
+		return andView;
+	}
+	
+	
+	@PostMapping("**/pesquisaPessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		 	
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		
+		andView.addObject("pessoas", PessoaRepository.findPessoaByName(nomepesquisa));
+		andView.addObject("pessoaobj", new Pessoa());
+		
 		return andView;
 	}
 }
